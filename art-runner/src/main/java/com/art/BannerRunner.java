14e8bf8f5e3985d282837235307266df476e8716
xuhao <690278565@qq.com>
2026-01-13 15:41:24 +0800

    feat(banner): 在启动横幅中显示版本信息
    - 注入 BuildProperties 以获取构建版本信息
    - 修改 BannerRunner 构造函数以接收 BuildProperties 参数
    - 在启动成功消息中添加当前版本号显示
    - 配置 build.gradle 以启用 spring-boot-build-info 插件
    - 添加版本信息到服务启动成功的控制台输出中

--- art-runner/src/main/java/com/art/BannerRunner.java
+++ art-runner/src/main/java/com/art/BannerRunner.java
@@ -6,24 +6,39 @@ import java.nio.file.Path;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 /**
  * 服务启动成功后打印
- * 
+ *
  * @author Luminous.X
  * @since 0.0.1-SNAPSHOT
  */
 @Component
 public class BannerRunner implements ApplicationRunner {
+    /**
+     * 构建信息
+     */
+    private final BuildProperties buildProperties;
+
+    /**
+     * 构造方法
+     *
+     * @param buildProperties 构建信息
+     */
+    public BannerRunner(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        String version = buildProperties.getVersion();
         Path bannerPath = new ClassPathResource("started-banner.txt").getFile().toPath();
         String bannerContent = Files.readString(bannerPath, StandardCharsets.UTF_8);
         System.out.println(bannerContent);
-        System.out.println("Art Engine服务启动成功(*^_^*)");
+        System.out.printf("Art Engine服务启动成功(*^_^*)        当前版本：%s%n", version);
     }
 
 }
