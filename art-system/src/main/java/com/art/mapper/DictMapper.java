628703fce04abedef4cd7bde3a1231444f91a5e8
徐皓 <690278565@qq.com>
2026-01-21 23:29:56 +0800

    feat: 添加字典Mapper接口
    - 创建DictMapper接口继承BaseMapper
    - 实现对Dict实体的基本CRUD操作支持
    - 集成MyBatis-Flex框架功能
    - 添加类级别文档注释
    - 包含作者和版本信息

---
+++ art-system/src/main/java/com/art/mapper/DictMapper.java
@@ -0,0 +1,13 @@
+package com.art.mapper;
+
+import com.art.domain.Dict;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 字典Mapper
+ *
+ * @author Luminous.X
+ * @since 0.0.1-SNAPSHOT
+ */
+public interface DictMapper extends BaseMapper<Dict> {
+}
