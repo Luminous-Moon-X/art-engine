8d1e14b7fa0ee204fdb68c2fe40f4a7ccb438a4f
徐皓 <690278565@qq.com>
2026-01-21 23:30:08 +0800

    feat(dict): 添加字典值数据访问层
    - 创建 DictValueMapper 接口继承 BaseMapper
    - 实现对 DictValue 实体的基本 CRUD 操作支持
    - 集成 MyBatis-Flex 框架进行数据持久化
    - 添加类级别注释文档和作者信息

---
+++ art-system/src/main/java/com/art/mapper/DictValueMapper.java
@@ -0,0 +1,13 @@
+package com.art.mapper;
+
+import com.art.domain.DictValue;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 字典值Mapper
+ *
+ * @author Luminous.X
+ * @since 0.0.1-SNAPSHOT
+ */
+public interface DictValueMapper extends BaseMapper<DictValue> {
+}
