3ed04ed95f6ea637d2bc305e29bd18f9e6c435bc
徐皓 <690278565@qq.com>
2026-01-21 23:29:32 +0800

    feat(dict): 添加字典表实体和视图对象
    - 创建了Dict实体类，包含字典名称、编码、类型、启用标识和备注字段
    - 配置了MyBatis-Flex注解映射到p_sys_dict表
    - 继承BaseEntity基类并添加了Lombok注解支持
    - 创建了DictVO视图对象类作为数据传输对象

---
+++ art-system/src/main/java/com/art/domain/Dict.java
@@ -0,0 +1,44 @@
+package com.art.domain;
+
+import com.art.common.BaseEntity;
+import com.mybatisflex.annotation.Column;
+import com.mybatisflex.annotation.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 字典表
+ *
+ * @author Luminous.X
+ * @since 0.0.1-SNAPSHOT
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Table("p_sys_dict")
+public class Dict extends BaseEntity {
+    /**
+     * 字典名称
+     */
+    @Column("dict_name")
+    private String dictName;
+    /**
+     * 字典编码
+     */
+    @Column("dict_code")
+    private String dictCode;
+    /**
+     * 字典类型
+     */
+    @Column("dict_type")
+    private String dictType;
+    /**
+     * 启用标识
+     */
+    @Column("enable_flag")
+    private String enableFlag;
+    /**
+     * 备注
+     */
+    @Column("remark")
+    private String remark;
+}
---
+++ art-system/src/main/java/com/art/domain/vo/DictVO.java
@@ -0,0 +1,4 @@
+package com.art.domain.vo;
+
+public class DictVO {
+}
